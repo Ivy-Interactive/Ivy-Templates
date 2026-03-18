d4a13885959dfbda8159e382fca5872a6ec83778
rorychatt <pavel.rinne@gmail.com>
2026-03-18 11:00:26 +0100

    feat: update hello template to use slnx, server.UseCulture, and updated Program.cs

--- hello/Program.cs
+++ hello/Program.cs
@@ -1,11 +1,12 @@
+using Ivy;
 using Hello.Apps;
-CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+
 var server = new Server();
+server.UseCulture("en-US");
 #if DEBUG
 server.UseHotReload();
 #endif
 server.AddAppsFromAssembly();
 server.AddConnectionsFromAssembly();
-var chromeSettings = new ChromeSettings().DefaultApp<HelloApp>().UseTabs(preventDuplicates: true);
-server.UseChrome(chromeSettings);
+server.UseChrome(new ChromeSettings().DefaultApp<HelloApp>().UseTabs(preventDuplicates: true));
 await server.RunAsync();
